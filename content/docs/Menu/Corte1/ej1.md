0bfc8c3fae2d5437b1d50a2f92f1445d2568a9a3
Juan Guillermo Sierra <jgsierraa@unal.edu.co>
2023-03-06 09:42:45 -0500

    chore: add another optical illusion and formating

--- content/docs/Menu/Corte1/ej1.md
+++ content/docs/Menu/Corte1/ej1.md
@@ -1,3 +1,12 @@
-# Ejercicio 1 Corte 1
+# Ejercicio 1 
 
+Study, implement and discuss possible applications of some known visual phenomena and optical illusions.
+
+## Optical Ilussions
+
+### Simple
+
+{{< p5-iframe sketch="/visualcomp/sketches/OpticalIllusion.js" width="725" height="425" >}}
+
+### Complex
 {{< p5-iframe sketch="/visualcomp/sketches/FlyingFive.js" width="725" height="425" >}}
---
+++ content/sketches/OpticalIllusion.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+      
+    // Do you notice any patterns here?
+      line(0, 0, 50, 400);
+      line(0, 50, 100, 400);
+      line(0, 100, 150, 400);
+    line(0, 150, 200, 400);
+    line(0, 200, 250, 400);
+    line(0, 250, 300, 400);
+    line(0, 300, 350, 400);
+    line(0, 350, 400, 400);
+      
+  }
