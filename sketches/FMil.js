aa9432f210f160b4b3e388db1976ba1f50898144
FabianMontes <FabianMontes@users.noreply.github.com>
2023-03-15 04:51:49 +0000

    deploy: db0423932326bb886998fc35ca4b038d5cb05db4

--- sketches/FMil.js
+++ sketches/FMil.js
@@ -1,7 +1,8 @@
-var angs,lines,xx,yy,len,cLen,moves,speeds,alphass,figse,figso;
+var angs,lines,xx,yy,len,cLen,moves,speeds,figse,figso,alphass,figui,figguis,lineU,lineD;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  
   moves=0;
   way=false;
   angs=0;
@@ -14,25 +15,19 @@ function setup() {
   speeds=2;
   figse = false;
   figso = true;
-  figui = createCheckbox('Figure', false);
+  figui = new CheckBox('Figure',0,height-20, false);
   
   figui.changed(Figs);
-  figui.position(0,height-figui.size().height);
-  figui.style('color','white');
+  figui.color(color(255,255,255));
   
-  figguis = createCheckbox('Fondo', true);
+  figguis = new CheckBox('Fondo',0,height-40, true);
   figguis.changed(Figso);
-  figguis.position(0,height-figui.size().height*2);
-  figguis.style('color','white');
+  figguis.color(color(255,255,255));
   
-  alphass=createSlider(0, 100, 0,2);
-  alphass.position(width-alphass.size().width,height-alphass.size().height);
+  alphass=new Slider('Color',width-100,height-50,0, 100, 0,2);
+  lineU = new   Button('+line',0,0) ;
+  lineD = new Button('-line',0,20)
   
-  lineU = createButton('+line') ;
-  lineD = createButton('-line') ;
-  
-  lineU.position(0,0);
-  lineD.position(width-lineD.size().width,0);
   lineU.mousePressed(LineUp);
   lineD.mousePressed(LineDown);
   stroke(100);
@@ -44,13 +39,12 @@ function setup() {
 function draw() {
   background(0);
   beginShape();
-  texti();
   if(figso){
     stroke(100);
   }else{
     noStroke()
   }
-  noFill();
+  noFill()
   for(var i=0;i<lines*2;i++){
     if(lines==1){
       break;
@@ -92,6 +86,16 @@ function draw() {
   
   
   cicles()
+  buttons()
+}
+
+
+function buttons(){
+  alphass.draw();
+  figui.draw();
+  figguis.draw();
+  lineU.draw();
+  lineD.draw();
 }
 
 function cicles(){
@@ -100,6 +104,7 @@ function cicles(){
     moves=0;
   }
 }
+
 function Fig(xy,dif){
   
   porct=sin(moves+dif);
@@ -137,10 +142,13 @@ function LineDown(){
   }
   updateAng();
 }
+
 function updateAng(){
   angs=PI/lines;
 }
+
 function Figs(){
+  
   figse = !figse
 }
 
@@ -148,11 +156,153 @@ function Figso(){
   figso = !figso
 }
 
-function texti(){
-  noStroke()
-  fill(50);
-  rect(width-alphass.size().width,height- alphass.size().height*2,alphass.size().width,alphass.size().height*3)
-  textSize(13);
-  fill(255);
-  text('Color', width-alphass.size().width,height- alphass.size().height*2);
+function mouseReleased(){
+  alphass.released();
+  figui.released();
+  figguis.released();
+  lineU.released();
+  lineD.released();
 }
+
+function mousePressed(){
+  alphass.Pressed();
+  figui.Pressed();
+  figguis.Pressed();
+  lineU.Pressed();
+  lineD.Pressed();
+  
+}
+
+function mouseDragged(){
+  alphass.Dragged()
+}
+
+class Button{
+    constructor(label,x,y){
+	this.x=x
+	this.y=y
+    this.w=10*label.length;
+    this.h=20;
+	this.label=label
+    this.colore = color(200, 200, 200);
+    this.isPressed= false
+    this.alpha= function() {}
+    }
+	draw(){
+      noStroke()
+      var bart = this.colore
+      if(this.isPressed){
+        bart = lerpColor(bart, color(0,0,0), 0.5);
+      }
+      fill(bart)
+      rect(this.x,this.y,this.w,this.h)
+      fill(0)
+      textWidth(1)
+      text(this.label,this.x+this.w/10,this.y+this.h/3*2)
+    }
+  
+    mousePressed(alpha = null){
+      this.alpha=alpha
+    }
+  
+    Pressed(){
+      if(mouseX>this.x && mouseX<this.x+this.w &&mouseY>this.y && mouseY<this.y+this.h){
+        this.isPressed=true;
+        this.alpha()  
+      }
+      
+    }
+  
+    released(){
+      this.isPressed=false;
+    }
+    color(colos){
+      this.colore= colos
+    }
+}
+
+class CheckBox extends Button{
+   constructor(label,x,y,ini){
+    super(label,x,y)
+    this.w=this.w*4/3
+    this.check=ini
+    }
+  
+    changed(alpha = null){
+      this.alpha=alpha
+    }
+  
+    draw(){
+      noStroke()
+      var bart = 255
+      if(this.check){
+        bart = 10
+      }
+      fill(bart)
+      rect(this.x,this.y,this.w/4,this.h)
+      fill(this.colore)
+      textWidth(1)
+      text(this.label,this.x+this.w/3,this.y+this.h/3*2)
+    }
+    Pressed(){
+      if(mouseX>this.x && mouseX<this.x+this.h &&mouseY>this.y && mouseY<this.y+this.h){
+        this.isPressed=true;
+      }
+    }
+    released(){
+      if(this.isPressed){
+        this.isPressed=false;
+        this.check=!this.check
+          this.alpha()
+      }
+    }
+}
+
+class Slider extends Button{
+  constructor(label,x,y,ini,end,value,step){
+   super(label,x,y)
+    this.w= max(this.w,(end-ini)+2)
+    this.h= this.h/3*5
+    this.ini = ini
+    this.end = end
+    this.step = step
+    this.valuee = value
+    this.colore = color(50, 50, 50);
+    this.alpha= function() {}
+  }
+  draw(){
+    noStroke()
+    fill(50);
+    rect(this.x,this.y,this.w,this.h)
+    textSize(13);
+    fill(255);
+    text('Color', this.x,this.y+this.h/5*2);
+    fill(100);
+    rect(this.x,this.y+this.h/2,this.w-1,this.h/2)
+    fill(this.colore);
+    rect(this.x+this.valuee/(this.end-this.ini)*this.w,this.y+this.h/2,this.w*this.step/(this.end-this.ini),this.h/2)
+  }
+  
+  value(){
+    return this.valuee
+  }
+  
+  Pressed(){
+      if(mouseX>this.x && mouseX<this.x+this.w &&mouseY>this.y+this.h/2 && mouseY<this.y+this.h){
+        this.isPressed=true;
+        
+      }
+      
+    }
+  
+  Dragged(){
+    if(this.isPressed){
+      this.valuee = map(mouseX,this.x,this.x+this.w,0,(this.end-this.ini)/this.step,true)
+      this.valuee = int(this.valuee*this.step+this.ini)
+      print(this.valuee)
+      
+    }
+  }
+}
+
+
